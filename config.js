5c97a1e356903d94e0be242b7e8c2e4c82df8de2
juanrama <115603059+juanrama@users.noreply.github.com>
2023-04-07 14:23:08 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,32 @@
+
+var config = {
+    texts: [
+        "Selamat Ulang Tahun",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
+        "YULI",  // 同上...
+        "Ini Contoh",
+        "Ini Contoh",
+        "Ini Contoh",
+        "Ini Contoh",
+        "Ini Contoh",
+        "Ini Contoh",
+        "Ini Contoh",
+        "Ini Contoh",
+        "Ini Contoh",
+    ],
+    
+    imgs: {
+        "YULI": "./imgs/pekok.jpg",
+        // "今天是你的生日": "./imgs/birthday.jpg",
+    },
+    // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
+    desc: {
+        turn_on: "NYALAIN WOY",
+        play: "MULAI HEHE",
+        bannar_coming: "KOK SEPI",
+        balloons_flying: "DEKORIIN DONG",
+        cake_fadein: "MANA KUENYA ？",
+        light_candle: "LILINNYA KO GAADA ？",
+        wish_message: "SELAMAT ULANG TAHUN SAYANG",
+        story: "A MESSAGE FOR YOU",
+    }
+};
